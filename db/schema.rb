cf8f640d330b3a413c93a6bc8d790eb51561d268
martin-yang <yangjunfeng@yangjunfengtekiMacBook-Air.local>
2014-07-03 18:10:35 +0800

    build task date table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -8,6 +8,15 @@ ActiveRecord::Schema.define(version: 20140708222957) do
     t.string "content"
     t.string "ancestry"
     t.string "state"
+    t.integer "task_date_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "task_dates", force: true do |t|
+    t.integer "year"
+    t.integer "month"
+    t.integer "day"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
