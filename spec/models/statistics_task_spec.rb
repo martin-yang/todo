d6152b85ff97f92160b661971afce56373a77752
martin-yang <yangjunfeng@yangjunfengtekiMacBook-Air.local>
2014-07-04 10:59:29 +0800

    modify statistics_task_spec of take tasks number of different state

--- spec/models/statistics_task_spec.rb
+++ spec/models/statistics_task_spec.rb
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 
-describe Statistics::Task do
+describe Statistics::OfTask do
+  before do
+    FactoryGirl.create(:task)
+  end
+
   it "take tasks number of different state by now time" do
-    Statistics::Task.by_today.should == {date: "2014-7-3",task_sum: 2,going: 2,over: 0}
+    Statistics::OfTask.by_date(Time.local(2014,7,3)).should == {date: "2014-07-03",task_sum: 1,going: 1,over: 0}
   end
 end
