17c66b50e1a770f3a4250b61e561302858f0c75d
martin-yang <yangjunfeng@yangjunfengtekiMacBook-Air.local>
2014-07-20 17:51:40 +0800

    delete padrino

--- models/taks_date.rb
+++
@@ -1,9 +0,0 @@
-class TaskDate < ActiveRecord::Base
-  attr_accessible :year, :month, :day, :created_at, :updated_at
-  has_many :tasks
-
-  # parameter date -->{year: year,month: month,day: day}
-  def self.tasks_of(date)
-    self.where(date).first.tasks rescue []
-  end
-end
--- spec/models/first_spec.rb
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-require 'spec_helper'
-
-describe "first test" do
-  it "first test" do
-    1.should == 1
-  end
-end
