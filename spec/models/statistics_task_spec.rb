b8a4886e9b7d2354c41ed8c790ea648a71f2fbee
yangjunfeng <408290909@qq.com>
2014-07-30 23:14:03 +0800

    build take tasks info in date test

--- spec/models/statistics_task_spec.rb
+++ spec/models/statistics_task_spec.rb
@@ -7,10 +7,10 @@ describe Statistics::OfTask do
   end
 
   it "take tasks number of different state by now time" do
-    Statistics::OfTask.by_date(Time.local(2014,7,3)).should == {date: "2014-07-03",task_sum: 1,going: 1,over: 0}
+    Statistics::TakeTasksNumByDate.new(Time.local(2014,7,3)).execute("take_today").should == {date: "2014-07-03",task_sum: 1,going: 1,over: 0}
   end
 
   it "take tasks number of different state by current month" do
-    Statistics::OfTask.by_month(Time.local(2014,7,3)).first.should == {date: "2014-07-01",task_sum: 0,going: 0,over: 0}
+    Statistics::TakeTasksNumByDate.new(Time.local(2014,7,3)).execute("take_month").first.should == {date: "2014-07-01",task_sum: 0,going: 0,over: 0}
   end
 end
