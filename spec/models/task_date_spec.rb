bc2d0d946b11bcf1700e581f04c69a6d866350d1
yangjunfeng <408290909@qq.com>
2014-07-30 23:22:03 +0800

    modify build query condition for date style

--- spec/models/task_date_spec.rb
+++ spec/models/task_date_spec.rb
@@ -6,10 +6,10 @@ describe TaskDate do
     FactoryGirl.create(:task)
   end
   it "take tasks in now time" do
-    TaskDate.tasks_of(BuildTaskDate.build(Time.local(2014,7,3))).size.should == 1
+    TaskDate.tasks_of(Search::TakeTasksByDate.new(Time.local(2014,7,3)).attributes).size.should == 1
   end
 
   it "search tasks by time" do
-    TaskDate.tasks_of(BuildTaskDate.build(Time.local(2014,7,2))).size.should == 0
+    TaskDate.tasks_of(Search::TakeTasksByDate.new(Time.local(2014,7,2)).attributes).size.should == 0
   end
 end
