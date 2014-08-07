0f0e8069e64b00350052c6209ac1742999793310
yangjunfeng <408290909@qq.com>
2014-08-07 10:56:00 +0800

    add I18n load path in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
+I18n.load_path << Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+I18n.locale = :zh_cn
+
 RSpec.configure do |config|
   # ## Mock Framework
   #
