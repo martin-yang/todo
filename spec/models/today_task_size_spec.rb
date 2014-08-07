01b5a687089555f10e61a610e1e9e2184a565e14
yangjunfeng <408290909@qq.com>
2014-08-07 10:25:55 +0800

    test take today task size return data style

---
+++ spec/models/today_task_size_spec.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe "TodayTaskSizeSpec" do
+  before do
+    FactoryGirl.create(:task)
+  end
+
+  it "take today Task size" do
+    Search::TakeTasksByDate.new(Time.local(2014,7,3)).execute.should == {date: "2014-07-03",tasks: [{id: 1,content: "root task",state: "new",children: [id: 2,content: "first task children",state: "new"]}]}
+  end
+
+end
