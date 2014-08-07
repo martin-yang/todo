dfcc52368884420158bdd9118261971478b9a2b4
yangjunfeng <408290909@qq.com>
2014-08-07 16:41:50 +0800

    setting send mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Todo::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address:              'smtp.163.com',
+  port:                 25,
+  domain:               'www.163.com',
+  authentication:       'login',
+  user_name:            'task_platform@163.com',
+  password:             'yang408290909',
+  enable_starttls_auto: false  }
