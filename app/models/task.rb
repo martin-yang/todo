58299313ed7632a2abe4f52e05dd10b3f99f9509
yangjunfeng <408290909@qq.com>
2014-08-07 10:30:16 +0800

    delete has_ancestry

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ActiveRecord::Base
-  has_ancestry
   attr_accessible :content,:state,:task_date,:task_date_id,:created_at,:updated_at
   belongs_to :task_date
 
@@ -9,11 +8,5 @@ class Task < ActiveRecord::Base
     event :over do
       transition :new => :over
     end
-  end
-
-  def delete_include_children
-    return self.delete if self.children.size.eql?(0)
-    self.children.delete_all
-    self.delete
   end
 end
