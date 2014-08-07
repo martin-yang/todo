6456428cd1cfd1cf14e2dfdec08aa5f4015ca1b4
yangjunfeng <408290909@qq.com>
2014-08-07 12:06:51 +0800

    modify take_tasks_by_date.rb,refactoring take tasks method

--- app/services/search/take_tasks_by_date.rb
+++ app/services/search/take_tasks_by_date.rb
@@ -3,11 +3,9 @@ module Search
     attr_accessor :year
     attr_accessor :month
     attr_accessor :day
-    attr_accessor :date
 
     # date type is datetime
     def initialize(date)
-      self.date = date
       self.year = date.year
       self.month = date.month
       self.day = date.day
@@ -15,11 +13,10 @@ module Search
 
     def execute
       tasks = ::TaskDate.where(self.attributes).first.tasks rescue []
-      def tasks.by_state(key)
-        return [] if self.size.eql?(0)
-        build_tasks_data_style(self.where(state: key))
+      def tasks.by_state(state)
+        self.where(state: state) rescue []
       end
-      build_tasks_data_style(tasks)
+      tasks      
     end
 
     def attributes
@@ -28,10 +25,6 @@ module Search
 
     def task_data_style(task)
       {id: task.id,content: task.content,state: (I18n.t "task.state.#{task.state}")}
-    end
-
-    def build_tasks_data_style(tasks)
-      {date: date.strftime('%Y-%m-%d'),tasks: tasks.map{|task| task_data_style(task)}}
     end
   end
 end
