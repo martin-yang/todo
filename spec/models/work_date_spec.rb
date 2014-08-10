ddf02888c3da432d665332aac9dff3ed1ce69a18
yangjunfeng <408290909@qq.com>
2014-08-10 17:32:33 +0800

    take today off duty date

---
+++ spec/models/work_date_spec.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe WorkDate do
+  before do
+    FactoryGirl.create(:work_date)
+  end
+
+  it "take off duty" do
+    WorkDate.take_off_duty_date.eql?("#{Time.now.strftime('%Y-%m-%d')} 18:00:00")
+  end
+end
