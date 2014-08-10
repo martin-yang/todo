0d0005ddfddb4493588c82165110c07e2addcd36
yangjunfeng <408290909@qq.com>
2014-08-10 17:39:09 +0800

    modify call now_time day

--- app/models/work_date.rb
+++ app/models/work_date.rb
@@ -1,3 +1,8 @@
-class WorkDate
+class WorkDate < ActiveRecord::Base
   attr_accessible :start_hour, :end_hour, :created_at, :updated_at
+
+  def take_off_duty_date
+    now_time = Time.now
+    Time.local(now_time.year,now_time.month,now_time.day,self.end_hour,0,0).strftime('%Y-%m-%d %H:%M:%S')
+  end
 end
