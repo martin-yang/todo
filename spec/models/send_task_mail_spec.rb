dd5d26ad7f02359397f3008b83078da0f96e577e
yangjunfeng <408290909@qq.com>
2014-08-10 17:04:35 +0800

    modify task email

--- spec/models/send_task_mail_spec.rb
+++ spec/models/send_task_mail_spec.rb
@@ -4,6 +4,9 @@ require 'spec_helper'
 describe "SendTaskMail" do
   before do
     FactoryGirl.create(:task)
+    20.times do |num|
+      Task.create(content: "TASKkkkkkkkkkkkkkkkkklkjalksdjflasjdflkajslkdfjlaksdjflkads#{num+1}",task_date: TaskDate.first)
+    end
   end
 
   it "send mail" do
