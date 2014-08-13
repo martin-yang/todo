533c3bf2c2054315cf81b3eacb2ff87281f34e89
yangjunfeng <408290909@qq.com>
2014-08-13 18:58:00 +0800

    add take today task complete detail in today_notice method

--- app/mailers/task_terminal.rb
+++ app/mailers/task_terminal.rb
@@ -3,7 +3,8 @@ class TaskTerminal < ActionMailer::Base
 
   def today_notice
     @today_tasks = BuildTodayTasksDataStyle.execute(Time.local(2014,7,3))
-
+    @today_complete_detail = Statistics::TakeTasksNumByDate.new(Time.local(2014,7,3)).execute("take_today")
+    
     mail(to: "408290909@qq.com", subject: (I18n.t "email.title",today: Time.local(2014,7,3).strftime("%Y-%m-%d")))
   end
 end
