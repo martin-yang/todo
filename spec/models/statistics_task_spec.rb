1f6ad62647651a1731e302e78aaef71614526cb5
yangjunfeng <408290909@qq.com>
2014-08-13 18:55:36 +0800

    add take today tasks complete detail spec

--- spec/models/statistics_task_spec.rb
+++ spec/models/statistics_task_spec.rb
@@ -13,4 +13,17 @@ describe "statistics tasks" do
   it "take tasks number of different state by current month" do
     Statistics::TakeTasksNumByDate.new(Time.local(2014,7,3)).execute("take_month").first.should == {date: "2014-07-01",task_sum: 0,going: 0,over: 0}
   end
+
+  describe "take task complete detail" do
+    before do
+      3.times do |num|
+        task = Task.create(content: "TASKkkkkkkkkkkkkkkkkklkjalksdjflasjdflkajslkdfjlaksdjflkads#{num+1}",task_date: TaskDate.first)
+        task.over
+      end
+    end
+
+    it "take today tasks complete detail" do
+      Statistics::TakeTasksNumByDate.new(Time.local(2014,7,3)).execute("take_today").should == {date: "2014-07-03",task_sum: 4,going: 1,over: 3}
+    end
+  end
 end
